a3bf82d259617b609611184bc90d74daecdae76f
arturvrgs <arturvargaslg@gmail.com>
2026-03-30 19:33:31 -0300

    muda ordem de chamada

--- ArturVillanovaVargas/src/Aula04/tarefa/src/main/java/com/artur/controller/TarefaController.java
+++ ArturVillanovaVargas/src/Aula04/tarefa/src/main/java/com/artur/controller/TarefaController.java
@@ -1,13 +1,43 @@
 package Aula04.tarefa.src.main.java.com.artur.controller;
 
 import Aula04.tarefa.src.main.java.com.artur.model.Tarefa;
+import Aula04.tarefa.src.main.java.com.artur.view.TarefaView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class TarefaController {
+
     private List<Tarefa> tarefas = new ArrayList<>();
+    private TarefaView view = new TarefaView();
+
+    public void iniciar() {
+
+        while (true) {
+
+            int opcao = view.apresentarMenu();
+
+            switch (opcao) {
+
+                case 1:
+                    String descricao = view.lerDescricaoTarefa();
+                    adicionarTarefa(descricao);
+                    break;
+
+                case 2:
+                    view.mostrarTarefas(listarTarefas());
+                    break;
+
+                case 3:
+                    int id = view.lerIdTarefa();
+                    concluirTarefa(id);
+                    break;
+
+                default:
+                    view.mostrarMensagem("Opção inválida!");
+            }
+        }
+    }
 
     public void adicionarTarefa(String descricao) {
         tarefas.add(new Tarefa(tarefas.size(), descricao, false));
@@ -17,12 +47,13 @@ public class TarefaController {
         return tarefas.toString();
     }
 
-    // record imutavel, ent tem que trocar por uma copia
     public void concluirTarefa(int id) {
+
         for (int i = 0; i < tarefas.size(); i++) {
+
             Tarefa tarefa = tarefas.get(i);
 
-            if(tarefa.id() == id) {
+            if (tarefa.id() == id) {
                 tarefas.set(i, new Tarefa(tarefa.id(), tarefa.descricao(), true));
             }
         }
--- ArturVillanovaVargas/src/Aula04/tarefa/src/main/java/com/artur/view/TarefaView.java
+++ ArturVillanovaVargas/src/Aula04/tarefa/src/main/java/com/artur/view/TarefaView.java
@@ -1,74 +1,39 @@
 package Aula04.tarefa.src.main.java.com.artur.view;
 
-import Aula04.tarefa.src.main.java.com.artur.controller.TarefaController;
-import Aula04.tarefa.src.main.java.com.artur.model.Tarefa;
 import java.util.Scanner;
 
 public class TarefaView {
 
     private Scanner scanner = new Scanner(System.in);
-    private final TarefaController controller;
 
-    public TarefaView(TarefaController controller) {
-       this.controller = controller;
+    public int apresentarMenu() {
+        System.out.println("------------------------");
+        System.out.println("1 - Adicionar tarefas");
+        System.out.println("2 - Listar tarefas");
+        System.out.println("3 - Concluir tarefa");
+        System.out.println("------------------------");
+        System.out.println("Escolha uma opção: ");
+
+        return Integer.parseInt(scanner.nextLine());
     }
 
-    public void apresentarMenu() {
-        while(true) {
-            System.out.println("------------------------");
-            System.out.println("1 - Adicionar tarefas");
-            System.out.println("2 - Listar tarefas");
-            System.out.println("3 - Concluir tarefa");
-            System.out.println("------------------------");
-            System.out.println("Escolha uma opção: ");
-
-            escolherOpcao(Integer.parseInt(this.scanner.nextLine()));
-        }
+    public String lerDescricaoTarefa() {
+        System.out.println("Digite a descrição da tarefa:");
+        return scanner.nextLine();
     }
 
-    private void apresentarAdicionarTarefa() {
-        System.out.println("Digite a descrição da tarefa: ");
-        String descricao = this.scanner.nextLine();
-
-       adicionarTarefa(descricao);
+    public int lerIdTarefa() {
+        System.out.println("Digite o id da tarefa:");
+        return Integer.parseInt(scanner.nextLine());
     }
 
-    private void apresentarListarTarefas() {
-        System.out.println("Tarefas: ");
+    public void mostrarTarefas(String tarefas) {
+        System.out.println("Tarefas:");
         System.out.println("-----------------");
-        System.out.println(listarTarefas());;
+        System.out.println(tarefas);
     }
 
-    private void apresentarConcluirTarefa() {
-        System.out.println("Digite o id da tarefa que deseja concluir: ");
-        int id = Integer.parseInt(this.scanner.nextLine());
-
-        concluirTarefa(id);
-    }
-
-    private void escolherOpcao(int opcao) {
-        switch (opcao) {
-            case 1: apresentarAdicionarTarefa();
-                break;
-            case 2: apresentarListarTarefas();
-                break;
-            case 3: apresentarConcluirTarefa();
-                break;
-            default:
-                System.out.println("Opção inválida!");
-                break;
-        }
-    }
-
-    private void adicionarTarefa(String descricao) {
-        controller.adicionarTarefa(descricao);
-    }
-
-    private String listarTarefas() {
-        return controller.listarTarefas();
-    }
-
-    private void concluirTarefa(int id) {
-        controller.concluirTarefa(id);
+    public void mostrarMensagem(String mensagem) {
+        System.out.println(mensagem);
     }
 }
