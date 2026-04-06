238bf604afe96a0226209a27cd53f42359a57081
gabriel furlan <gabriel.unisinos@gmail.com>
2026-04-06 19:23:50 -0300

    Tarefa Aula 5 Livros

---
+++ src/TarefaAula4/App.java
@@ -0,0 +1,12 @@
+package TarefaAula4;
+import TarefaAula4.TarefaController;
+
+public class App {
+    public static void main(String[] args) {
+
+        TarefaController controller = new TarefaController();
+        controller.iniciar();
+
+    }
+}
+
